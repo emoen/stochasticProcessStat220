ecfd0a4a3372088f4a52a77d7dff943be1bcb85f
emoen <endremoen@hotmail.com>
2019-09-21 21:16:43 +0200

    4.4

--- hw3/hw_3.r
+++ hw3/hw_3.r
@@ -13,6 +13,7 @@ myx = seq(1,200,1)
 Ay = dexp(myx-1, rate=1/100)
 By = dexp(myx-1, rate=1/900)
 Zy = dexp(myx-1, rate=1/1000)
+Zy = dexp(myx-1, rate=9/10)
 plot(myx, By, type="l", col="red")
 lines(Zy, co="blue")
 lines(Ay, co="green")
@@ -34,16 +35,36 @@ U_idx = U_output$ix
 Z_sorted = Z[order(U_idx)]
 V = c(1:m)
 Y = c(1:m)
-for (j in 1:n){
-    for (i in 1:m) {
+for (j in 1:m){
+    for (i in 1:n) {
         V[j] = V[j] + U_sort[m*(j-1)+i]
         Y[j] = Y[j] + Z_sorted[m*(j-1)+i]        
     }
     V[j] = (1/m)*V[j]
     Y[j] = (1/m)*Y[j]
 }
-plot(c(1:m), Y, type="l", col="red")
-lines(Y, col="blue")
+plot(Y,V)
+
+#plot(c(1:m), V, type="l", col="red")
+#lines(Y, col="blue")
+
+#4.5
+N=10^4
+blodtype = c(0.34,0.408, 0.068, 0.034, 0.06, 0.072, 0.012, 0.0006)
+accum_blodtype = 0
+n = c(0,0,0,0,0,0,0,0)
+p = c(0,0,0,0,0,0,0,0)
+X = c(0,0,0,0,0,0,0,0)
+size_accum = 0
+for (i in 1:8) {
+    p[i] = blodtype[i]/(1-accum_blodtype)
+    accum_blodtype = accum_blodtype + blodtype[i]
+    print(accum_blodtype)
+    n[i] = N - size_accum
+    size_accum = size_accum + n[i]
+    X[i] = rbinom(1, n[i], p[i])    
+}
+p
 
 
 
