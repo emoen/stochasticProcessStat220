3ae84a2507239fa043b915556ac5a12358a00647
emoen <endremoen@hotmail.com>
2019-10-02 10:18:47 +0200

    p)

--- hw4/hw_4r.r
+++ hw4/hw_4r.r
@@ -99,6 +99,7 @@ for (i in 2:L) {
 #tau_time
 #  1   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  20  21  23  26  33  38 
 #126  13   4   9   2   5   5   4   1   4   8   4   2   1   6   2   2   1   1   2   1   2   1 
+hist(tau_time)
 
 mean_X0 = sum(tau_time)/(L-1)
 #4.742718
@@ -112,12 +113,98 @@ mean_X0 = sum(tau_time)/(L-1)
 Y<-pmin(pmax(0, X-2),1)
 plot(1:n, Y, type='l')
 
-# l)
+# l) rain days in bergen
 z = rep(0, n)
-z[1] = y[1]
+z[1] = Y[1]
 for (i in 2:n) {
-    
+    z[i] = (z[i-1]+1)*Y[i]
 }
+plot(1:n, z)
+
+# m) find piHat with n=50*365 (50 years)
+###################
+# copy - past from top
+n=50*365
+X2 = c(rep(0,n+1))
+X2[1]<-sample(S,1, prob=init )
+k=1
+for (k in 2:(n+1)) {
+    X2[k]<-sample(S,1, prob=mP[X2[k-1],] )
+}
+X2 = X2[-1]
+plot(1:n, X2, type="s", col="blue")
+
+piHat2 = table(X2)/n
+
+#####################
+# piHat with 50 years is similar to 1000 days:
+#> piHat2
+#X2
+#        1         2         3         4 
+#0.2041096 0.1356164 0.1356164 0.5246575 
+#> piHat
+#X
+#    1     2     3     4 
+#0.207 0.138 0.137 0.518 
+
+# n) 
+#get pi from eigen vector with eigen value = 1
+e = eigen(mP)
+left_vector = ginv(e$vectors)
+pi_eig = left_vector[1,]/sum(left_vector[1,])
+sum(pi_eig)
+#> pi_eig
+#[1] 0.1943463 0.1342756 0.1342756 0.5371025
+#> piHat2
+#X2
+#        1         2         3         4 
+#0.2041096 0.1356164 0.1356164 0.5246575 
+##############
+#pi_eig is close to piHat2
+#############
+
+# o) rainiest days in 50 years according to model
+Y2<-pmin(pmax(0, X2-2),1)
+z2 = rep(0, n)
+z2[1] = Y2[1]
+for (i in 2:n) {
+    z2[i] = (z2[i-1]+1)*Y2[i]
+}
+max(z2)
+# z2 = 36 vs z=24", # 103 regndager registert i 1986
+
+# p)
+L50<-length(X2[X2==1])
+tau50<-order(X2)[1:L50]
+tau_time2 = rep(0, L50-1)
+wait_time2 = tau50[1]
+for (i in 2:L50) {
+    tau_time2[i-1] = tau50[i] - wait_time2
+    wait_time2 = tau50[i]
+} 
+d = density(tau_time2)
+hist(tau_time2)
+plot(d, col="red")
+
+#> mean(tau_time2)
+#[1] 4.942818
+mean_expo = mean(tau_time2)
+
+#mean = 1/rate
+rate_expo = 1/mean_expo)
+hist(rexp(L50-1, rate_expo))
+expo_x = (1:(L50-1))
+expo_y = dexp((1:length(tau_time2)), rate=(rate_expo))
+#plot(x=expo_x, y=expo_y, type='l')
+lines(expo_y)
+
+t2 = tau_time2[order(tau_time2)]
+#remove all 1's
+t3 = t2[2285:(L50-1)]
+plot(density(t3))
+plot(density(t3[1081:length(t3)]))
+
+#*********** q) see notes ********************
 
 
 
