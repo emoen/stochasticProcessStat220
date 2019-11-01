165b608142fa5e0492cb3d8f647c1373892f0cf9
Moen Endre <endre.moen@imr.no>
2019-11-01 16:55:55 +0100

    simulate poisson process from poisson distribution

--- hw6/hw6.r
+++ hw6/hw6.r
@@ -2,6 +2,12 @@
 #This algorithm exploits the fact that interarrival times are exponentially distributed. 
 #We simulate the arrival times until the maximum time horizon is achieved.
 
+# 2 methods for simulating poisson process
+# https://www.r-bloggers.com/%F0%9F%93%88-simulating-poisson-process-part-1/
+
+#link inter- arrival, arrival, and number of arrivals at time
+# https://towardsdatascience.com/the-poisson-process-everything-you-need-to-know-322aa0ab9e9a
+
 lambdaExp = 2
 t=10
 T = c(rep(0, t)) # inter-arrival time - exponential distributed
@@ -27,10 +33,27 @@ for (i in c(1:t)){
 }
 plot(c(1:t), X)
 
-########
+###########################
+#Method 2
+#This method simulates the number of jumps by Possion random variable 
+#with the rate equals to the product of the time horizon and the process’s rate. 
+#Then, to calculate arrival times, random variables with uniform distribution 
+#are generated and ordered after
+
 poissonProcess = c(rep(0,t))
+rates = c(rep(0,t))
 for ( i in c(1:t)) {
     rate = lambdaExp*i
     poissonProcess[i] = rpois(1, rate)
+    rates[i]=rate
 }
+poissonProcess
+rate
+
+jumpsNumber = c(rep(0,t))
+jumpsNumber[1]=poissonProcess[1]
+for ( i in c(2:t)){
+    jumpsNumber[i] = jumpsNumber[i-1]+poissonProcess[i]
+}
+jumpsNumber
 plot(c(1:t), poissonProcess)
